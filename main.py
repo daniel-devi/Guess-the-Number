6b23e8152fae6f96d0bf2a2dcf57d9a178cf5035
Daniel Agufenwa <devonaguh33@gmail.com>
2023-12-28 13:38:42 +0000

    attempts added

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ import random
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
+    attempts = 1
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
             print('Sorry, guess again. Too low.')
+            attempts = attempts+1
         elif guess > random_number:
             print('Sorry, guess again. Too high.')
+            attempts = attempts+1
 
-    print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
+    print(f'Yay, congrats. You have guessed the number {random_number} correctly!!, after {attempts} Tries')
 
 def computer_guess(x):
     low = 1
     high = x
     feedback = ''
-    attempts = 0
+    attempts = 1
     while feedback != 'c':
         if low != high:
             guess = random.randint(low, high)
